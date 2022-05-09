5bca9cca54cc45620afd2d41e70f49f663eb8f57
Alonso Bustos <alonso.bustos@spsa.pe>
2022-05-09 11:56:57 -0500

    MVCPass Template Fail

--- src/test/java/com/example/demo/impl/mvc/HttpRequestMVC.java
+++ src/test/java/com/example/demo/impl/mvc/HttpRequestMVC.java
@@ -2,24 +2,26 @@ package com.example.demo.impl.mvc;
 
 import com.example.demo.HttpRequest;
 import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @Component
-//@AutoConfigureMockMvc
 public class HttpRequestMVC implements HttpRequest {
-  //@Autowired
-  //private MockMvc mvc;
+  @Autowired
+  MockMvc mvc;
 
   @Override
   @SneakyThrows
   public String getContent(String url) {
-    return "Hello World";
-  /*
     return mvc.perform(get(url).accept(MediaType.APPLICATION_JSON))
       .andExpect(status().isOk())
       .andReturn()
       .getResponse()
       .getContentAsString();
-  */
   }
 }

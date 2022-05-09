02e2078dd5786f6878e016f7a7c862a59702164e
Alonso Bustos <alonso.bustos@spsa.pe>
2022-05-09 11:54:52 -0500

    TemplatePass MVC Fail

--- src/test/java/com/example/demo/impl/template/HttpRequestTemplate.java
+++ src/test/java/com/example/demo/impl/template/HttpRequestTemplate.java
@@ -1,21 +1,24 @@
 package com.example.demo.impl.template;
 
 import com.example.demo.HttpRequest;
+import lombok.val;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @Component
 //@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class HttpRequestTemplate implements HttpRequest {
-  //@Autowired
-  //TestRestTemplate template;
+  @Autowired
+  TestRestTemplate template;
 
   @Override
   public String getContent(String url) {
-    return "Hello World";
-    /*
     val response = template.getForEntity(url, String.class);
     assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
     return response.getBody();
-    */
   }
 }
